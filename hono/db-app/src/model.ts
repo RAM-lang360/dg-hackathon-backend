2490df55d0bedea3ebee5df74e0f9478e18a3925
RAM <eva1022moriya@gmail.com>
2025-11-10 05:42:27 +0000

    add type

--- hono/db-app/src/model.ts
+++ hono/db-app/src/model.ts
@@ -1,3 +1,7 @@
+type indicatorHL={
+    
+}
+
 class model {
     constructor() {
         this.indicatorHL = [];
---
+++ hono/db-app/src/types/datamodel.ts
@@ -0,0 +1,13 @@
+//カウントのカテゴリ
+export interface CountAndTotal {
+    count: number;
+    total: number;
+}
+//User種別のカテゴリ
+export type WeightCategory = "super_heavy" | "heavy" | "light" | "super_light";
+
+//1月当たりのUser種別の分類
+export type MonthlyWeightsData = {
+  [K in WeightCategory]: CountAndTotal;
+};
+
